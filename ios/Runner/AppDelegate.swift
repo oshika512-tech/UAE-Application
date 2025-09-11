4d93789bbeda4d7bdfb992d57968fa8f94d2ac6d
moshika38 <moshika38@gmail.com>
2025-09-11 18:45:50 +0530

    setup notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,24 @@
-import Flutter
 import UIKit
+import Flutter
+import awesome_notifications
+import shared_preferences_ios
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Background actions register කරන්න
+    SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
+      SwiftAwesomeNotificationsPlugin.register(
+        with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
+      FLTSharedPreferencesPlugin.register(
+        with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
